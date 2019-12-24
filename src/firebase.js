2281346b03fcdcc3569e8b2c7fadc2d98c1dab54
sanderdebr <38420946+sanderdebr@users.noreply.github.com>
2019-12-24 14:51:31 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCFi28yliKRVyEp0aBILUioUMuLp5IRUD0",
-    authDomain: "just-clock-it-16e29.firebaseapp.com",
-    databaseURL: "https://just-clock-it-16e29.firebaseio.com",
-    projectId: "just-clock-it-16e29",
-    storageBucket: "just-clock-it-16e29.appspot.com",
-    messagingSenderId: "852320179549",
-    appId: "1:852320179549:web:ac8cc355b4f6aac4c484b4"
-  };
+// Removed privacy
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
